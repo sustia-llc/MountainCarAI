93f45a564e9b6ce8249e159a195a78cbaca8133d
Holly Grimm <holly.grimm@gmail.com>
2024-09-13 16:12:33 -0600

    refactor create_agent_and_world; add run_simulation

--- src/environment.jl
+++ src/environment.jl
@@ -1,3 +1,5 @@
+using MountainCarAI
+
 mutable struct Environment
     T_ai::Int
     t::Int
@@ -5,9 +7,13 @@ mutable struct Environment
     execute_ai::Vector
     observe_ai::Vector
     agent_states::Vector{Vector{Float64}}  # Store each agent's state
-    
-    Environment(; T_ai, agents, execute_ai, observe_ai, initial_states) = begin
-        return new(T_ai, 1, agents, execute_ai, observe_ai, initial_states)
+    heights::Vector{Function}
+    x_target::Vector{Float64}
+    N_ai::Int
+    Fas::Vector{Function}
+
+    Environment(; T_ai, agents, execute_ai, observe_ai, initial_states, heights, x_target, N_ai, Fas) = begin
+        return new(T_ai, 1, agents, execute_ai, observe_ai, initial_states, heights, x_target, N_ai, Fas)
     end
 end
 
@@ -32,3 +38,72 @@ function step_environment!(env::Environment)
     end
     env.t += 1
 end
+
+function create_agent_and_world(engine_force_limit, initial_position, initial_velocity, T_ai, x_target)
+    Fa, Ff, Fg, height = create_physics(engine_force_limit = engine_force_limit)
+    execute_ai, observe_ai, initial_state = create_world(
+        (Fa, Ff, Fg, height),
+        initial_position = initial_position, 
+        initial_velocity = initial_velocity
+    )
+    (compute_ai, act_ai, slide_ai, future_ai) = create_agent(
+        T = T_ai, Fa = Fa, Fg = Fg, Ff = Ff,
+        engine_force_limit = engine_force_limit,
+        x_target = x_target,
+        initial_position = initial_position,
+        initial_velocity = initial_velocity
+    )
+    agent = Dict(
+        :act_ai => act_ai, :future_ai => future_ai, :compute_ai => compute_ai, :slide_ai => slide_ai,
+        :agent_a => Float64[], :agent_f => Vector{Float64}[], :agent_x => Vector{Float64}[]
+    )
+    return agent, execute_ai, observe_ai, initial_state, height, Fa
+end
+
+function run_simulation(; N_ai=100, return_env=false)
+    # Set up simulation parameters
+    T_ai = 50
+    x_target = [0.5, 0.0]
+
+    initial_position = -0.5
+    initial_velocity = 0.0    
+
+    engine_force_limit1 = 0.04
+    engine_force_limit2 = 0.02
+
+    # Create two agents and corresponding worlds
+    # TODO: return a tuple for the world, similar to agent
+    agent1, execute_ai1, observe_ai1, initial_state1, height1, Fa1 = create_agent_and_world(
+        engine_force_limit1, initial_position, initial_velocity, T_ai, x_target
+    )
+    agent2, execute_ai2, observe_ai2, initial_state2, height2, Fa2 = create_agent_and_world(
+        engine_force_limit2, initial_position, initial_velocity, T_ai, x_target
+    )
+
+    # Initialize the environment
+    env = Environment(
+        T_ai = T_ai,
+        agents = [agent1, agent2],
+        execute_ai = [execute_ai1, execute_ai2],
+        observe_ai = [observe_ai1, observe_ai2],
+        initial_states = [initial_state1, initial_state2],
+        heights = [height1, height2],
+        x_target = x_target,
+        N_ai = N_ai,
+        Fas = [Fa1, Fa2]
+    )
+
+    # Step through actions for each agent within the environment
+    for _ in 1:N_ai
+        step_environment!(env)
+    end
+
+    if return_env
+        return env
+    else
+        # Return final positions of both agents
+        return [env.agents[1][:agent_x][end][1], env.agents[2][:agent_x][end][1]]
+    end
+end
+
+run_simulation()
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,61 +11,24 @@ using Test
         
         engine_force_limit1 = 0.04
         engine_force_limit2 = 0.02
-        physics1 = create_physics(engine_force_limit = engine_force_limit1)
-        physics2 = create_physics(engine_force_limit = engine_force_limit2)
-        
-        execute_ai1, observe_ai1, initial_state1 = create_world(physics1, initial_position = initial_position, initial_velocity = initial_velocity)
-        execute_ai2, observe_ai2, initial_state2 = create_world(physics2, initial_position = initial_position, initial_velocity = initial_velocity)
-        
-        agent_funcs1 = create_agent(
-            T = T_ai,
-            Fa = physics1[1],
-            Fg = physics1[3],
-            Ff = physics1[2],
-            engine_force_limit = engine_force_limit1,
-            x_target = x_target,
-            initial_position = initial_position,
-            initial_velocity = initial_velocity
+
+        agent1, execute_ai1, observe_ai1, initial_state1, height1, Fa1 = create_agent_and_world(
+            engine_force_limit1, initial_position, initial_velocity, T_ai, x_target
         )
-        
-        agent_funcs2 = create_agent(
-            T = T_ai,
-            Fa = physics2[1],
-            Fg = physics2[3],
-            Ff = physics2[2],
-            engine_force_limit = engine_force_limit2,
-            x_target = x_target,
-            initial_position = initial_position,
-            initial_velocity = initial_velocity
+        agent2, execute_ai2, observe_ai2, initial_state2, height2, Fa2 = create_agent_and_world(
+            engine_force_limit2, initial_position, initial_velocity, T_ai, x_target
         )
-        
-        agents = [
-            Dict(
-                :act_ai => agent_funcs1[2], 
-                :future_ai => agent_funcs1[4], 
-                :compute_ai => agent_funcs1[1], 
-                :slide_ai => agent_funcs1[3],
-                :agent_a => Float64[],
-                :agent_f => Vector{Float64}[],
-                :agent_x => Vector{Vector{Float64}}(undef, N_ai)
-            ),
-            Dict(
-                :act_ai => agent_funcs2[2], 
-                :future_ai => agent_funcs2[4], 
-                :compute_ai => agent_funcs2[1], 
-                :slide_ai => agent_funcs2[3],
-                :agent_a => Float64[],
-                :agent_f => Vector{Float64}[],
-                :agent_x => Vector{Vector{Float64}}(undef, N_ai)
-            )
-        ]
-        
+
         env = Environment(
             T_ai = T_ai,
-            agents = agents,
+            agents = [agent1, agent2],
             execute_ai = [execute_ai1, execute_ai2],
             observe_ai = [observe_ai1, observe_ai2],
-            initial_states = [initial_state1, initial_state2]
+            initial_states = [initial_state1, initial_state2],
+            heights = [height1, height2],
+            x_target = x_target,
+            N_ai = N_ai,
+            Fas = [Fa1, Fa2]
         )
         
         @test env.T_ai == T_ai
@@ -77,78 +40,8 @@ using Test
     end
     
     @testset "Environment Simulation" begin
-        T_ai = 50
-        N_ai = 100
-        x_target = [0.5, 0.0]
-        initial_position = -0.5
-        initial_velocity = 0.0
-        
-        engine_force_limit1 = 0.04
-        engine_force_limit2 = 0.02
-        physics1 = create_physics(engine_force_limit = engine_force_limit1)
-        physics2 = create_physics(engine_force_limit = engine_force_limit2)
-        
-        execute_ai1, observe_ai1, initial_state1 = create_world(physics1, initial_position = initial_position, initial_velocity = initial_velocity)
-        execute_ai2, observe_ai2, initial_state2 = create_world(physics2, initial_position = initial_position, initial_velocity = initial_velocity)
-        
-        agent_funcs1 = create_agent(
-            T = T_ai,
-            Fa = physics1[1],
-            Fg = physics1[3],
-            Ff = physics1[2],
-            engine_force_limit = engine_force_limit1,
-            x_target = x_target,
-            initial_position = initial_position,
-            initial_velocity = initial_velocity
-        )
-        
-        agent_funcs2 = create_agent(
-            T = T_ai,
-            Fa = physics2[1],
-            Fg = physics2[3],
-            Ff = physics2[2],
-            engine_force_limit = engine_force_limit2,
-            x_target = x_target,
-            initial_position = initial_position,
-            initial_velocity = initial_velocity
-        )
-        
-        agents = [
-            Dict(
-                :act_ai => agent_funcs1[2], 
-                :future_ai => agent_funcs1[4], 
-                :compute_ai => agent_funcs1[1], 
-                :slide_ai => agent_funcs1[3],
-                :agent_a => Float64[],
-                :agent_f => Vector{Float64}[],
-                :agent_x => Vector{Vector{Float64}}(undef, N_ai)
-            ),
-            Dict(
-                :act_ai => agent_funcs2[2], 
-                :future_ai => agent_funcs2[4], 
-                :compute_ai => agent_funcs2[1], 
-                :slide_ai => agent_funcs2[3],
-                :agent_a => Float64[],
-                :agent_f => Vector{Float64}[],
-                :agent_x => Vector{Vector{Float64}}(undef, N_ai)
-            )
-        ]
-        
-        env = Environment(
-            T_ai = T_ai,
-            agents = agents,
-            execute_ai = [execute_ai1, execute_ai2],
-            observe_ai = [observe_ai1, observe_ai2],
-            initial_states = [initial_state1, initial_state2]
-        )
-        
-        for _ in 1:N_ai
-            step_environment!(env)
-        end
-
-        final_position1 = env.agents[1][:agent_x][end][1]
-        final_position2 = env.agents[2][:agent_x][end][1]
-        @test isapprox(final_position1, 0.72, atol=0.01)
-        @test isapprox(final_position2, -0.33, atol=0.01)
+        final_positions =  run_simulation()
+        @test isapprox(final_positions[1], 0.72, atol=0.01)
+        @test isapprox(final_positions[2], -0.33, atol=0.01)
     end
 end
